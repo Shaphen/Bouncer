328828b55502e1c2e04bf264ae60d0653d1912cc
Shaphen <swpburn@gmail.com>
2020-05-18 17:59:50 -0700

    Add generation ability for platforms

--- src/01_moving_object.js
+++ src/01_moving_object.js
@@ -2,21 +2,31 @@
 // let mo = new MovingObject({
 //   pos: [30, 30],
 //   vel: [10, 10],
-//   length: 10,
-//   rad: 5,
-//   color: "#00FF00",
+//   width: 100,
+//   height: 5,
+//   color: "black",
 // });
 
 function MovingObject(options) {
   this.pos = options.pos; // ex. [30, 30]
   this.vel = options.vel; // ex. [10, 10]
-  this.length = options.length; // ex. 5
-  this.rad = options.rad; // ex. 5
+  this.width = options.width; // ex. 5
+  this.height = options.height; // ex. 5
   this.color = options.color // ex. "#00FF00"
 }
 
-MovingObject.prototype.draw = (ctx) => {
-  
+MovingObject.prototype.draw = function(ctx) {
+  ctx.fillStyle = this.color
+  ctx.beginPath();
+
+  ctx.rect(
+    this.pos[0],
+    this.pos[1],
+    this.width,
+    this.height
+  );
+
+  ctx.fill();
 }
 
 module.exports = MovingObject;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 console.log("webpack is working!")
 const MovingObject = require("./01_moving_object");
-
 window.MovingObject = MovingObject;
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  let canvas = document.getElementById("game-canvas");
+  let ctx = canvas.getContext("2d")
+  window.canvas = canvas;
+  window.ctx = ctx;
+});
