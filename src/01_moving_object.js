2b1e649ba46d929c631948ae10889eb4267da972
Shaphen <swpburn@gmail.com>
2020-05-18 18:04:20 -0700

    Add moving function for moving object

--- src/01_moving_object.js
+++ src/01_moving_object.js
@@ -1,7 +1,7 @@
 // test code
 // let mo = new MovingObject({
 //   pos: [30, 30],
-//   vel: [10, 10],
+//   vel: [10, 0],
 //   width: 100,
 //   height: 5,
 //   color: "black",
@@ -27,6 +27,13 @@ MovingObject.prototype.draw = function(ctx) {
   );
 
   ctx.fill();
+}
+
+MovingObject.prototype.move = function(ctx) {
+  let newPos = [];
+  newPos.push(this.pos[0] + this.vel[0]);
+  newPos.push(this.pos[1] + this.vel[1]);
+  this.pos = newPos;
 }
 
 module.exports = MovingObject;
